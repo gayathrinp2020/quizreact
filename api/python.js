b8b89aeb86830bee64fd9ebe02c2266b545c624b
Gayathri <gayathrinair@carestack.com>
2023-06-01 16:28:39 +0530

    define multiple data function

--- api/python.js
+++ api/python.js
@@ -1,5 +1,14 @@
 const python = require("./data/pythonq");
 
+const fetchQuiznames = () => {
+  // Assuming pythonq.fetchQuiznames() returns a promise
+  return pythonq.fetchQuiznames();
+};
+
+const fetchQuestions = () => {
+  // Assuming pythonq.fetchQuestions() returns a promise
+  return pythonq.fetchQuestions();
+};
 const handler = async (req, res) => {
   const [data1, data2] = await Promise.all([
     fetchQuiznames(),
